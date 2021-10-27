e9c4ac5c15a4b9de6a755d37fd709d1d6e52805b
dec0deit <piyushsingla2416@gmail.com>
2021-10-28 00:13:03 +0530

    added sample code

--- app.js
+++ app.js
@@ -1,42 +1,51 @@
 require('dotenv').config()
+
 const express = require('express')
 const nodemailer = require("nodemailer");
+const bodyParser = require('body-parser')
 const app = express()
-app.get('/sendMessageAndEmail', async(req, res) => {
-  console.log(process.env.EMAIL,process.env.PASSWORD)
- 
-  let transporter = nodemailer.createTransport(
-  {
-    service:'gmail',
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASSWORD,
-    }
-  });
- 
-  try{
-    let info = await transporter.sendMail({
-      from: 'TEST EMAIL', // sender address
-      to: "singlanipun29@gmail.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    });
 
-    console.log(info)
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    res.status(200).json({message:'Success'});
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: false}))
+  
+app.post('/sendMessageAndEmail', async(req, res) => {
+  console.log(req.body)
+  const {email,phoneNo} =req.body;
+  if(email && phoneNo){
+      console.log(process.env.EMAIL,process.env.PASSWORD)
+      let transporter = nodemailer.createTransport(
+      {
+        service:'gmail',
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASSWORD,
+        }
+      });
+      try{
+        let info = await transporter.sendMail({
+        from: 'TEST EMAIL',
+        to: [email,phoneNo], 
+        subject: "Hello ✔", 
+        text: "Hello world?", 
+        html: "<b>Hello world?</b>", 
+        });
+        console.log(info)
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        res.status(200).json({message:'Success'});
+    }
+    catch(err){
+      console.log(err)
+      res.status(401).json({message : 'Bad Request'})
+    }
   }
-  catch(err){
-    console.log(err)
-    res.status(401).json({message : 'Bad Request'})
+  else{
+    res.status(401).json({message : 'Enter Proper Email and Phone Number'})
   }
 })
 app.get('/', (req, res) => {
-    res.status(200).json({
-        message:'Home Page'});
-  })
+    res.status(200).json({message:'Home Page'});
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
