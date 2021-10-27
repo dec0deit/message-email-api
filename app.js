405ef6a622eceb4e05475d32b620efe3442d1ddb
dec0deit <piyushsingla2416@gmail.com>
2021-10-27 13:21:11 +0530

    deployed api

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ app.get('/sendMessageAndEmail', (req, res) => {
   res.status(200).json({
       message:'Success'});
 })
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message:'Home Page'});
+  })
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
