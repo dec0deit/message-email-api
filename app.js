a9c27cafeafb8b47804d3113832c065573adbacb
dec0deit <piyushsingla2416@gmail.com>
2021-11-05 00:16:20 +0530

    added code for message and email

--- app.js
+++ app.js
@@ -8,18 +8,19 @@ const path = require("path");
 const pdf = require("html-pdf");
 const jwt = require('jsonwebtoken');
 const app = express()
-const dirPath = path.join(__dirname, "public/pdfs");
 const axios = require('axios')
 
+jwt.sign("AA","AAA")
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
+// require the Twilio module and create a REST client
+const client = require('twilio')(accountSid, authToken);
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true}))
 
-const files = fs.readdirSync(dirPath).map(name => {
-  return {
-    url: `localhost:3000/pdfs/${name}`
-  };
-});
+
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -30,7 +31,8 @@ app.get('/template',(req,res)=>{
 
 app.post('/sendMessageAndEmail', async(req, res) => {
   console.log(req.body);
-  const {name,email_id,mobile_no} =req.body.transaction.customer;
+  const {name,email_id,mobile_no} =(req.body?.transaction?.customer);
+  const amount = req.body.gross_amount;
   if(email_id && mobile_no){
       console.log(process.env.EMAIL,process.env.PASSWORD)
       let transporter = nodemailer.createTransport(
@@ -51,26 +53,23 @@ app.post('/sendMessageAndEmail', async(req, res) => {
           from: process.env.EMAIL,
           to: [email_id], 
           subject: "Thanks For Donating", 
-          text: `Hello ${name} Thanks a lot for donating.`, 
+          text: `Hello ${name} Thanks a lot for donating. Please download your certificate at `, 
         });
         console.log(info)
         console.log("Message sent: %s", info.messageId);
         console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        
-        const headers = {
-          'Content-Type':'application/json',
-          'X-API-Key' : process.env.API_KEY
-        }
-
-        const body = {
-          'mobile' : '+91'+mobile_no,
-          'text' : `Hello ${name} Thanks a lot for Donating`
-        }
-
-        const apiResponse = await axios.post('https://api.ycloud.com/v1/sms/send_messages',body,{headers});
-        
-        console.log(apiResponse.data);
-        res.status(200).json({message:'Success'});
+      
+        client.messages
+        .create({
+          to: '+91' + mobile_no,
+          from: '+15306758417',
+          body: `Hello ${name} Thanks for donating. Please download your certificate at  `,
+        })
+        .then(message =>{
+           console.log(message);
+           console.log(message.sid)
+           res.status(200).json({message:'Success'});
+        });
     }
     catch(err){
       console.log(err)
@@ -88,7 +87,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/getCertificate', (req, res) => {
-
+  console.log(req.query)
   let donorInfo = {
     Name : jwt.decode(req.query.Name),
     Amount : jwt.decode(req.query.Amount),
@@ -113,7 +112,7 @@ app.get('/getCertificate', (req, res) => {
   });
 
 });
-
+  
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
